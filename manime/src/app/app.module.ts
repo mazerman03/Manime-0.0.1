a4bece948ecc6cad7183202dc5999144db385215
Max Zermeño Manrique <105751347+mazerman03@users.noreply.github.com>
2024-05-24 23:32:50 -0600

    Firebase Config Template
    Firebase Config for modules and config.

--- manime/src/app/app.module.ts
+++ manime/src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { initializeApp } from "firebase/app";
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { initializeApp as initializeApp_alias, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, AngularFireModule.initializeApp(environment.firebaseConfig)],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"manime-8695f","appId":"1:1003321030986:web:3e3e19bafb481a6861e9b3","storageBucket":"manime-8695f.appspot.com","apiKey":"AIzaSyB-6i6XJ4UUzFMODyg4AuxcduKQDXq0b9Y","authDomain":"manime-8695f.firebaseapp.com","messagingSenderId":"1003321030986"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideFunctions(() => getFunctions()), provideStorage(() => getStorage())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- manime/src/app/tab3/tab3.page.ts
+++ manime/src/app/tab3/tab3.page.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JikanService } from '../projects/api/service/jikan.service';
+import { Anime } from 'src/models/anime-data.model';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
+  animeContainerList: Anime[] = [];
+  appCardContainer: any = [];
+  LibraryContainerList: number[] = [];
 
-  constructor() {}
+  page: number=1;
+  has_next_page: boolean = true;
+  loadingCurrentEventData: any;
 
+  constructor(private service: JikanService) {}
+
+  ngOnInit(): void {
+    this.initializeLibraryAnimeContainerList();
+    this.initializeLibraryContainer();
+  }
+
+  initializeLibraryContainer() {
+    this.service.getLibraryListTEST().forEach((mal_id: number) => {this.LibraryContainerList.push(mal_id);  });
+  }
+  initializeLibraryAnimeContainerList(){
+    
+    this.LibraryContainerList.forEach((libraryEl: number) => 
+      this.service.getAnimeById(libraryEl).subscribe(animeEl => {
+        
+        animeEl.data.forEach(element => {
+          this.appCardContainer.push({
+            mal_id: element.mal_id,
+            title: element.title,
+            image: element.images.jpg.large_image_url,
+            score: element.score,
+            modelItem: element,
+          });
+        })
+    }));
+  }
+    
+     
+
+
+    
+/* 
+    loadData(event: any) {
+      this.page = this.page + 1;
+      this.loadingCurrentEventData = event;
+      this.loadLibraryAnimeContainerList();
+    } */
 }
